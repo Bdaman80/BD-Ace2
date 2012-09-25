56c2372f2ea49aeb2cf41dca85fb3569225d9454
Bdaman <bdaman80@gmail.com>
2012-09-25 04:46:37 -0500

    Add Gov's SmartassV2, Interactive, InteractiveX, and sync up all of the git-ignored files

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Tue Sep 25 03:51:50 CDT 2012"
+#define LINUX_COMPILE_TIME "03:51:50"
+#define LINUX_COMPILE_BY "bdaman"
+#define LINUX_COMPILE_HOST "Height03"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
